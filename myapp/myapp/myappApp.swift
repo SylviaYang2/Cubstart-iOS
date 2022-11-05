94d7e59b928639a7fcd49b1808aef78ec1f37e58
SylviaYang2 <sylviayang252@berkeley.edu>
2022-11-04 21:16:50 -0700

    finished hw7

---
+++ myapp/myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by Sylvia Yang on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
